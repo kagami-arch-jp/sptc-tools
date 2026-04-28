774c2a9c1a226e871f1200a9665ccb9ac6015477
111 <111>
2026-04-28 21:18:02 +0900

    bugfix

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@
 process.env.ENV='PROD'
 process.argv=[
   ...process.argv.slice(0, 2),
-  ...'-d -rindex.s -w.'.split(' '),
+  '-d',
+  '-rindex.s',
+  '--workdir',
+  __dirname,
 ]
 require('sptc/bin/sptcd')
 require('open').default('http://127.0.0.1:9090')
